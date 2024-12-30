04011238f6b361ae51d5be079b82d8031310d5ff
Jair Boggio <2954022+jairboggio@users.noreply.github.com>
2024-12-30 07:21:14 +0100

    feat: create hero

--- wordpress/wp-content/themes/vida/front-page.php
+++ wordpress/wp-content/themes/vida/front-page.php
@@ -12,12 +12,54 @@
  * @package vida
  */
 
+$theme = get_stylesheet_directory_uri();
+$heroBg = $theme."/assets/images/placeholders/home-hero-background.jpg";
+
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main layout">
+		<section class="c-hero full-width layout">
+			<?php //if($fields['hero']['image']): ?>
+			<!-- <picture class="c-hero__bg full-width">
+				<?php //if($fields['hero']['image_mobile']): ?>
+				<source srcset="<?php //echo $fields['hero']['image_mobile'] ?>" media="(max-width: 767px)" />
+				<?php //endif; ?>
+				<img src="<?php //echo $fields['hero']['image']; ?>" alt="">
+			</picture> -->
+			<?php //endif; ?>
+			<picture class="c-hero__bg full-width">
+				<!-- <source srcset="" media="(max-width: 767px)" /> -->
+				<img src="<?php echo $heroBg; ?>" alt="">
+			</picture>
 
-
+			<!-- <div class="c-hero__inner">
+				<div class="c-hero__content">
+					<?php //if(trim($fields['hero']['caption'] ?? '')): ?>
+					<p class="c-hero__caption"><?php //echo $fields['hero']['caption']; ?></p>
+					<?php //endif; ?>
+					<?php //if(trim($fields['hero']['description'] ?? '')): ?>
+					<p class="c-hero__desc"><?php //echo $fields['hero']['description']; ?></p>
+					<?php //endif; ?>
+					<?php
+						/* if($fields['hero']['button']):
+							$heroButton = $fields['hero']['button']; */
+					?>
+					<a href="<?php //echo $heroButton['url']; ?>" target="<?php //echo $heroButton['target']; ?>" class="button c-hero__button"><?php //echo $heroButton['title']; ?></a>
+					<?php //endif; ?>
+				</div>
+			</div> -->
+			<div class="c-hero__inner">
+				<div class="c-hero__content">
+					<h2 class="c-hero__title heading1">Titular</h2>
+					<p class="c-hero__desc text1">Lorem ipsum dolor sit amet consectetur. Scelerisque tempor nibh nibh sit eleifend ut tortor id urna.</p>
+					<div class="c-hero__buttons">
+						<a href="#" target="_blank" class="button button--fs-constant">Application Pack</a>
+						<a href="#" target="_blank" class="button button--fs-constant">Application Pack</a>
+					</div>
+				</div>
+			</div>
+		</section>
 	</main><!-- #main -->
 
 <?php
