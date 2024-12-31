6c675c2a4d39a94b6ef50d6161fec8218ae2f76d
Jair Boggio <2954022+jairboggio@users.noreply.github.com>
2024-12-31 05:50:35 +0100

    feat: create home variable section

--- wordpress/wp-content/themes/vida/front-page.php
+++ wordpress/wp-content/themes/vida/front-page.php
@@ -17,6 +17,7 @@ $heroBg = $theme."/assets/images/placeholders/home-hero-background.jpg";
 $associatedPartnersLogo = $theme."/assets/images/placeholders/associated-partners-logo.png";
 $bannerBg = $theme."/assets/images/placeholders/home-banner-background.jpg";
 $bannerBgMobile = $theme."/assets/images/placeholders/home-banner-background-mobile.jpg";
+$variableSectionImage = $theme."/assets/images/placeholders/home-variable-section.jpg";
 
 get_header();
 ?>
@@ -165,6 +166,23 @@ get_header();
 					<p class="c-hero__desc text1">Lorem ipsum dolor sit amet consectetur. Scelerisque tempor nibh nibh sit eleifend ut tortor id urna.</p>
 					<div class="c-hero__buttons">
 						<a href="#" target="_blank" class="button button--transparent button--fs-constant">Apply</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="c-section">
+			<div class="c-info c-cols" data-template="1-2">
+				<div class="col1">
+					<h2 class="c-info__title heading2 uppercase">Variable section</h2>
+					<div class="c-info__desc text2">
+						<p>One of VHIO's main missions is to train and inspire the next generation of leading cancer researchers and medical doctors in Oncology to drive research and innovation and tackle the societal challenges of tomorrow.</p>
+						<p>Through the VHIO Academy established in 2021, the institute aims to attract young talent globally and provide state-of-the-art training and career development opportunities.</p>
+					</div>
+					<a href="#" target="_blank" class="button button--fs-constant">The project</a>
+				</div>
+				<div class="col2">
+					<div class="c-info__image">
+						<img src="<?php echo $variableSectionImage; ?>" alt="">
 					</div>
 				</div>
 			</div>
