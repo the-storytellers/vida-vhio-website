2e12c37de1edbaed4215d549cdebc02ac9887683
Jair Boggio <2954022+jairboggio@users.noreply.github.com>
2024-12-31 06:32:07 +0100

    feat: create template for Associated Partners

---
+++ src/associated-partners.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Template Name: Associated Partners
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package vida
+ */
+
+$theme = get_stylesheet_directory_uri();
+$associatedPartnersLogo = $theme."/assets/images/placeholders/associated-partners-logo.png";
+
+get_header();
+?>
+
+	<main id="primary" class="site-main layout">
+		<section class="c-section">
+			<div class="c-section__header">
+				<h1 class="c-section__title heading1">Associated Partners</h1>
+				<div class="c-section__desc text1 text1--fs-constant">
+					<p>Lorem ipsum dolor sit amet consectetur. Platea mollis ipsum dolor justo. Urna volutpat nam molestie mattis justo diam amet tortor. Malesuada ullamcorper feugiat arcu ultrices non odio.</p>
+				</div>
+			</div>
+			<div class="c-grid">
+				<div class="c-card">
+					<div class="c-card__image">
+						<img src="<?php echo $associatedPartnersLogo; ?>" alt="">
+					</div>
+					<div class="c-card__content">
+						<div class="c-card__primary">
+							<h3 class="c-card__title heading3 uppercase">Lorem Ipsum</h3>
+							<div class="c-card__desc text2">
+								<p>Lorem ipsum dolor sit amet consectetur. Tellus pharetra molestie in nulla tincidunt cursus tellus. Tellus fringilla duis lobortis sit feugiat risus.</p>
+							</div>
+							<div class="c-card__secondary">
+								<a href="#" class="button">Ver más</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="c-card">
+					<div class="c-card__image">
+						<img src="<?php echo $associatedPartnersLogo; ?>" alt="">
+					</div>
+					<div class="c-card__content">
+						<div class="c-card__primary">
+							<h3 class="c-card__title heading3 uppercase">Lorem Ipsum</h3>
+							<div class="c-card__desc text2">
+								<p>Lorem ipsum dolor sit amet consectetur. Tellus pharetra molestie in nulla tincidunt cursus tellus. Tellus fringilla duis lobortis sit feugiat risus.</p>
+							</div>
+							<div class="c-card__secondary">
+								<a href="#" class="button">Ver más</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="c-card">
+					<div class="c-card__image">
+						<img src="<?php echo $associatedPartnersLogo; ?>" alt="">
+					</div>
+					<div class="c-card__content">
+						<div class="c-card__primary">
+							<h3 class="c-card__title heading3 uppercase">Lorem Ipsum</h3>
+							<div class="c-card__desc text2">
+								<p>Lorem ipsum dolor sit amet consectetur. Tellus pharetra molestie in nulla tincidunt cursus tellus. Tellus fringilla duis lobortis sit feugiat risus.</p>
+							</div>
+							<div class="c-card__secondary">
+								<a href="#" class="button">Ver más</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main><!-- #main -->
+
+<?php
+get_footer();
